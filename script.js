6e125b48a465914c356a46a6e76d26e4ba77cc0a
Sophia <sophia.lazaroni@gmail.com>
2024-04-22 14:58:33 +0100

    Highlight possible moves when clicked and add some style to the highlights

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const initialState = {
     blackTurn: true,
     piecesWhite: [0, 2, 4, 6, 9, 11, 13, 15, 16, 18, 20, 22],
     piecesBlack: [40, 42, 44, 46, 49, 51, 53, 55, 56, 58, 60, 62],
+    whitebox: [0, 2, 4, 6, 9, 11, 13, 15, 16, 18, 20, 22, 25, 27, 29, 31, 32, 34, 36, 38, 41, 43, 45, 47, 48, 50, 52, 54, 57, 59, 61],
+    blackBox: [1, 3, 5, 7, 8, 10, 12, 14, 17, 19, 21, 23, 24, 26, 28, 30, 33, 35, 37, 39, 40, 42, 44, 46, 49, 51, 53, 55, 56, 58, 60, 62],
 };
 
 init(root, initialState, update, view); 
@@ -30,9 +32,14 @@ function row(highlighted, rowNumber) {
         const properties = { 
             class: "box", 
             onClick: () => {
-                return isBlackPiece ? number : undefined;
+                if (isBlackPiece) {
+                    return number;
+
+                }
+                
             }, 
-            style: boxStyle(highlighted, number)
+            style: boxStyle(highlighted, number),
+
         };
         let box = h("div", properties, []);
         
@@ -69,8 +76,21 @@ function view(state) {
     ];
 }
 
-function boxStyle (n, boxNumber) {
-    return n == boxNumber ? "background-color: yellow" : "";
+function boxStyle (highlighted, boxNumber) {
+    
+    const number = boxNumber;
+    let isblackBox = initialState.blackBox.includes(number);
+
+    const isSelected = boxNumber == highlighted;
+    const isRelevant = boxNumber == (highlighted - 7);
+    const isRelevant2 = boxNumber == (highlighted - 9);
+    if (isSelected) {
+        return "background-color: #a37000; border: 3px double #FFFF00";
+    } else if (isRelevant && isblackBox) {
+        return "background-color: #7a9c59; border: 3px double #00FF00";
+    } else if (isRelevant2 && isblackBox){
+        return "background-color: #7a9c59; border: 3px double #00FF00";  
+    }
 }
 
 function boxNumber(rowNumber, columnNumber){
